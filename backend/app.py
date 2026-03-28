e4dac6ca346f52eb508a85795e5c2b1114dd9fbb
dabbiz11 <dabbiz11@gmail.com>
2026-03-28 11:26:27 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -127,5 +127,6 @@ def get_bookings():
     return jsonify(bookings)
 
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
